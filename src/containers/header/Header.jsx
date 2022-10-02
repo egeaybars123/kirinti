3433b26cfbb996cf8950bf8a814ba81ac901b289
egeaybars123 <egeaybars1102@gmail.com>
2022-10-02 10:25:24 +0300

    ui tweaks

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='kirinti_header section_padding' id="home">
        <div className='kirinti_header-content'>
          <h1 className='gradient_text'>Kırıntı sayesinde cüzdanlarınızda kalan "kırıntıları" süpürüyoruz.</h1>
-         <p>Avalanche C-Chain üzerinde yaklaşık olarak 3.5M adres bulunmakta ve bir çok cüzdanda küçük miktarlarda işe yaramaz tokenler bulunmakta "KIRINTI" sayesinde bu tokenlardan kurtulmanın yanında bağış atabileceksiniz.</p>
+         <p>Bugünün 'kırıntı'ları yarının projelerini inşa edecek! Cüzdanınızda bulunan, küçük miktarda ve kullanılmayan tokenlerinizi AVAX'a dönüştürmek ister misiniz?  "kırıntı" sayesinde bu tokenlardan kurtulmanın yanında çeşitli projelere ve kurumlara bağış yapabilirsiniz.</p>
        </div>
      <div className='kirinti_header-image'>
        <img src={token} alt="token"/>
